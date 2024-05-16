04ca32f0a3b738ea6681368321354b6fe3c07516
Jothin Kumar <contact@jothin.tech>
2024-05-16 16:22:22 +0530

    endpoint for get

---
+++ functions/get/get.js
@@ -0,0 +1,21 @@
+const catalyst = require('zcatalyst-sdk-node');
+module.exports = (context, basicIO) => {
+
+	const mazeId = basicIO.getArgument('maze-id');
+	if (!mazeId) {
+		basicIO.setStatus(400);
+		basicIO.write('Invalid maze ID');
+		context.close();
+		return;
+	}
+
+	const app = catalyst.initialize(context);
+	const datastore = app.datastore();
+	const table = datastore.table('maze_data');
+	table.getRow(parseInt(mazeId)).then((row) => {
+		const data = row["maze-data"]
+		const lvl = row["level"]
+		basicIO.write(lvl + data);
+		context.close();
+	})
+};
